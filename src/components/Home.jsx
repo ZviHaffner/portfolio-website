c336cc71154c560a352f83b79db906c57aad7e8c
Zvi Haffner <zvihaffner@gmail.com>
2024-08-22 16:45:15 +0100

    Writes a basic introduction for the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="border-2 border-blue-500 bg-blue-50 px-10 py-5">
+      <h1>Hello I'm Zvi Haffner</h1>
+      <h2>Junior Software Engineer</h2>
+      <p>
+        I am a passionate Full-Stack engineer focussed on Front-End, with a knack for turning complex
+        problems into elegant, efficient solutions. With expertise in
+        JavaScript, I specialise in developing robust applications that
+        prioritise both performance and user experience. Please explore my portfolio,
+        to see my projects and skills.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
