68795105c301d1b3824610edbbb9fc0972497813
Zvi Haffner <zvihaffner@gmail.com>
2024-09-15 17:39:40 +0100

    Adds image and edits text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,38 +4,57 @@ const About = () => {
   const [showCv, setShowCv] = useState(false);
 
   return (
-    <>
-      <div className="border-2 border-blue-500 bg-blue-50 px-10 py-5 text-left">
-        My name is Zvi Haffner and I am a Junior Software Developer based in
-        Manchester.
+    <div className="mx-60">
+      <img
+        src="profile-pic.png"
+        alt=""
+        width={300}
+        height={300}
+        className="object-contain rounded-md mx-auto my-10"
+      />
+      <article className="text-xl">
+        <p>
+          My name is Zvi Haffner and I am a Junior Software Developer based in
+          Manchester.
+        </p>
         <br />
-        My journey into the tech world really kicked off when I joined
-        Northcoders, an intensive bootcamp that gave me the skills and
-        confidence to start building real-world applications.
+        <p>
+          My journey into the tech world really kicked off when I joined
+          Northcoders, an intensive bootcamp that gave me the skills and
+          confidence to start building real-world applications.
+        </p>
         <br />
-        Whether it's crafting a sleek front-end with React or getting my hands
-        dirty with back-end work in Node.js and SQL, I’m all about creating
-        software that not only works well but feels intuitive to use.
+        <p>
+          Whether it's crafting a sleek front-end with React or getting my hands
+          dirty with back-end work in Node.js and SQL, I’m all about creating
+          software that not only works well but feels intuitive to use.
+        </p>
         <br />
-        Before I found my passion in coding, I spent a lot of time studying,
-        earning a BSc in Liberal Arts with a top GPA. I’ve also got a background
-        in Jewish law, which might sound a bit different from coding, but it’s
-        actually where I developed a love for problem-solving and deep
-        thinking—skills I now apply every day as a developer.
+        <p>
+          Before I found my passion in coding, I spent a lot of time studying,
+          earning a BSc in Liberal Arts with a top GPA. I’ve also got a
+          background in Jewish law, which might sound a bit different from
+          coding, but it’s actually where I developed a love for problem-solving
+          and deep thinking—skills I now apply every day as a developer.
+        </p>
         <br />
-        When I'm not coding, you may find me solving puzzles, catching up on the
-        latest in tech and history, or following the football and Formula 1
-        races. I also enjoy learning new things, whether through evening study
-        groups or binge-watching educational YouTube videos. If you’re curious
-        about my work or just want to chat about tech, feel free to connect with
-        me on LinkedIn or check out my projects on GitHub. Let’s connect and
-        maybe even build something cool together!
-      </div>
-      <button onClick={() => setShowCv(!showCv)} className="border-2 border-green-500 bg-green-50 p-2 rounded-md m-5">
+        <p>
+          If you’re curious about my work or just want to chat about tech, feel
+          free to connect with me on LinkedIn or check out my projects on
+          GitHub. Let’s connect and maybe even build something cool together!
+        </p>
+      </article>
+      <button
+        onClick={() => setShowCv(!showCv)}
+        className="border-2 border-green-500 bg-green-50 m-5 p-2 rounded-md"
+      >
         {showCv ? "HIDE " : "SHOW "}CV
       </button>
       {showCv ? (
-        <div style={{ width: "80%", height: "600px" }} className="flex justify-center items-center mx-auto">
+        <div
+          style={{ width: "100%", height: "600px" }}
+          className="flex justify-center items-center mx-auto"
+        >
           <embed
             src="/zvihaffner-cv.pdf"
             type="application/pdf"
@@ -44,7 +63,7 @@ const About = () => {
           />
         </div>
       ) : null}
-    </>
+    </div>
   );
 };
 
